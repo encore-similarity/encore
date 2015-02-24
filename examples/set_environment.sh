29310649c55cd94caccf97123ae5eb5662e77ad8
teo <teo@Diagon-Alley.(none)>
2015-02-24 15:19:43 +0100

    NP=3

--- examples/set_environment.sh
+++ examples/set_environment.sh
@@ -1,6 +1,6 @@
 export MDANALYSIS_BUILD=/home/username/MDAnalysis-0.8.1/build/lib.linux-x86_64-2.6/MDAnalysis/
 export ENCORE_BUILD="/home/teo/.local/lib/python2.7/site-packages/ENCORE-0.1b-py2.7-linux-x86_64.egg/encore/"
 export PYTHON=/usr/bin/python
-export NP=8
+export NP=3
 
 export PYTHONPATH=$ENCORE_BUILD:$PYTHONPATH
