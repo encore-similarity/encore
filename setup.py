0b9c613b0bffe05ed74a9b2d7d6e29ea25cf355d
teo <teo@Diagon-Alley.(none)>
2015-03-11 11:07:56 +0100

    - last fixes to documentation - removed useless/empty file

--- setup.py
+++ setup.py
@@ -8,36 +8,11 @@ ENCORE requires:
 	* a command-line C compiler (as gcc or clang)
 	* a working Cython distribution
 
-For a basic installation just type the command::
+For a basic installation just type the command:
 
-  python setup.py install
-
-
-  http://groups.google.com/group/mdnalysis-discussion
-
-
-By default we use setuptools <http://pypi.python.org/pypi/setuptools>.  The
-details of such an "EasyInstall" installation procedure are shown on
-
-  http://peak.telecommunity.com/DevCenter/EasyInstall
-
-By changing the code below you can also switch to a standard distutils
-installation.
+  python setup.py install --user
 """
 
-#------------------------------------------------------------
-# selection of the installation system
-#------------------------------------------------------------
-#
-# Standard distutils-based installation:
-#
-##from distutils.core import setup, Extension
-
-# setuptools ("EasyInstall") installation:
-#
-# If you want EasyInstall features then enable the next three lines and comment
-# out the preceding line 'from distutils.core import ...'
-#
 
 from ez_setup import use_setuptools
 use_setuptools()
@@ -90,44 +65,8 @@ except ImportError:
     use_cython = False
     cmdclass = {}
 
-def hasfunction(cc, funcname, include=None, extra_postargs=None):
-    # From http://stackoverflow.com/questions/
-    #            7018879/disabling-output-when-compiling-with-distutils
-    tmpdir = tempfile.mkdtemp(prefix='hasfunction-')
-    devnull = oldstderr = None
-    try:
-        try:
-            fname = os.path.join(tmpdir, 'funcname.c')
-            f = open(fname, 'w')
-            if include is not None:
-                f.write('#include %s\n' % include)
-            f.write('int main(void) {\n')
-            f.write('    %s;\n' % funcname)
-            f.write('}\n')
-            f.close()
-            # Redirect stderr to /dev/null to hide any error messages
-            # from the compiler.
-            # This will have to be changed if we ever have to check
-            # for a function on Windows.
-            devnull = open('/dev/null', 'w')
-            oldstderr = os.dup(sys.stderr.fileno())
-            os.dup2(devnull.fileno(), sys.stderr.fileno())
-            objects = cc.compile([fname], output_dir=tmpdir,
-                                 extra_postargs=extra_postargs)
-            cc.link_executable(objects, os.path.join(tmpdir, "a.out"))
-        except Exception as e:
-            return False
-        return True
-    finally:
-        if oldstderr is not None:
-            os.dup2(oldstderr, sys.stderr.fileno())
-        if devnull is not None:
-            devnull.close()
-        shutil.rmtree(tmpdir)
-
-
 if __name__ == '__main__':
-    RELEASE = "0.1b"     # NOTE: keep in sync with MDAnalysis.version in __init__.py
+    RELEASE = "0.7"     
 
     if 'DEBUG_CFLAGS' in os.environ:
         extra_compile_args = '\
